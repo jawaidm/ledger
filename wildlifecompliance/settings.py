80e409713f07ea1b9b2b98b9368dd271774f1883
Jawaid Jushtaq@ <jawaid_mushtaq@yahoo.com>
2019-01-03 09:06:39 +0800

    initial wc pdf

--- wildlifecompliance/settings.py
+++ wildlifecompliance/settings.py
@@ -5,6 +5,7 @@ ROOT_URLCONF = 'wildlifecompliance.urls'
 SITE_ID = 1
 
 INSTALLED_APPS += [
+    'django.contrib.humanize',
     'bootstrap3',
     'wildlifecompliance',
     'wildlifecompliance.components.main',
@@ -33,6 +34,9 @@ MIDDLEWARE_CLASSES += [
     'wildlifecompliance.middleware.FirstTimeNagScreenMiddleware'
 ]
 
+LATEX_GRAPHIC_FOLDER = os.path.join(BASE_DIR,"templates","latex","images")
+
+TEMPLATES[0]['DIRS'].append(os.path.join(BASE_DIR, 'templates'))
 TEMPLATES[0]['DIRS'].append(os.path.join(BASE_DIR, 'wildlifecompliance', 'templates'))
 TEMPLATES[0]['DIRS'].append(os.path.join(BASE_DIR, 'wildlifecompliance','components','organisations', 'templates'))
 TEMPLATES[0]['DIRS'].append(os.path.join(BASE_DIR, 'wildlifecompliance','components','emails', 'templates'))
@@ -72,7 +76,7 @@ DEV_STATIC = env('DEV_STATIC',False)
 DEV_STATIC_URL = env('DEV_STATIC_URL')
 if DEV_STATIC and not DEV_STATIC_URL:
     raise ImproperlyConfigured('If running in DEV_STATIC, DEV_STATIC_URL has to be set')
-DATA_UPLOAD_MAX_NUMBER_FIELDS = None 
+DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 
 # Department details
 SYSTEM_NAME = env('SYSTEM_NAME', 'Wildlife Licensing System')
